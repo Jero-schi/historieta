ba8047d28f9de15e15cbd8b2262ec1459af1e339
Jeronimo <jeronimocomaschi05@gmail.com>
2024-06-14 21:18:27 -0300

    ola

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA0d8x6mn-5pxHUhInu9MH5adTZp4NDnnE",
-    authDomain: "historieta-6edf4.firebaseapp.com",
-    projectId: "historieta-6edf4",
-    storageBucket: "historieta-6edf4.appspot.com",
-    messagingSenderId: "821994685057",
-    appId: "1:821994685057:web:74a1fac6c8aae48e904380"
-  };
+  apiKey: "AIzaSyCye-7_jzcedZlkOVkz5D9m1IGXYfWNyO8",
+  authDomain: "backend-comic.firebaseapp.com",
+  projectId: "backend-comic",
+  storageBucket: "backend-comic.appspot.com",
+  messagingSenderId: "358891442675",
+  appId: "1:358891442675:web:2f6796982a7162388b0286"
+};
 
 const app = initializeApp(firebaseConfig);
 
