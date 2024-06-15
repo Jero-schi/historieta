114c5aa50d6ddbabf8108f0f59f5d24e06b4ccb0
Jeronimo <jeronimocomaschi05@gmail.com>
2024-06-14 21:33:02 -0300

    l

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-// import outputPlugin from 'vite-plugin-output';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()
-    // outputPlugin({
-    //   outDir: 'dist'
-    // })
-  ],
-  base: '/historieta/',
+  base: './',
+  plugins: [react()],
 })
